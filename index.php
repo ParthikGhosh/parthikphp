4acf1558260752a3f040b55fb410821eab82d341
Parthik Ghosh <31651344+ParthikGhosh@users.noreply.github.com>
2018-02-26 12:47:21 +0530

    Update index.php
    changed header

--- index.php
+++ index.php
@@ -9,7 +9,6 @@ if ($connection->connect_errno) {
     printf("Connect failed: %s\n", $mysqli->connect_error);
     exit();
 }
-echo "<br><br><br><br>Welcome to Openshift :)";
-$rs->close();
+echo "<br><br><br><br><h2>Welcome to Openshift :)</h2><br><br><br><br>";
 $connection->close();
 ?>
