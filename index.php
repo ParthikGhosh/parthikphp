91e16ed89f326da27f823d4241d37c540fd05d4e
Parthik Ghosh <31651344+ParthikGhosh@users.noreply.github.com>
2018-03-26 12:02:11 +0530

    Update index.php
    Added connection successful msg.

--- index.php
+++ index.php
@@ -7,9 +7,10 @@ $dbname = getenv("DB_NAME");
 $connection = new mysqli($dbhost, $dbuser, $dbpwd, $dbname);
 if ($connection->connect_errno) {
     printf("<br><h2>Mysql backing service secrets missing...</h2><br>");
-    printf("<br><h2>Connect failed.<br><h2> %s\n", $mysqli->connect_error);
+    printf("<br><h2>Connect failed...<br><h2> %s\n", $mysqli->connect_error);
     exit();
 }
 echo "<br><h2>Mysql backing services provisioned via service broker is available...</h2><br>";
+echo "<br><h2>Connection Successful...</h2><br>";
 $connection->close();
 ?>
